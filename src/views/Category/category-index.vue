a2825ab9ed52836efeea9b1c5b8f4242f047ebbf
VMIAO-bx <212497659@qq.com>
2026-05-17 17:47:50 +0800

    一级分类--激活状态显示以及商品列表渲染

--- src/views/Category/category-index.vue
+++ src/views/Category/category-index.vue
@@ -3,12 +3,14 @@ import { getTopCategoryAPI } from "@/apis/category";
 import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
 import { getBannerAPI } from "@/apis/home";
+import GoodsItem from "../Home/components/GoodsItem.vue";
 //获取分类数据
 const categoryData = ref({});
 const route = useRoute();
 const getTopCategory = async () => {
   const res = await getTopCategoryAPI(route.params.id);
   categoryData.value = res.result;
+  console.log(categoryData);
 };
 //获取轮播图数据
 const bannerList = ref([0]);
@@ -43,6 +45,26 @@ onMounted(() => {
             <img :src="item.imgUrl" alt="" />
           </el-carousel-item>
         </el-carousel>
+      </div>
+      <!-- 全部分类 -->
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink to="/">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="good in item.goods" :good="good" :key="good.id" />
+        </div>
       </div>
     </div>
   </div>
@@ -123,15 +145,17 @@ onMounted(() => {
 
   .bread-container {
     padding: 25px 0;
-    .home-banner {
-      width: 1240px;
-      height: 500px;
-      margin: 0 auto;
-      img {
-        width: 100%;
-        height: 500px;
-      }
-    }
+  }
+}
+
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  margin: 0 auto;
+
+  img {
+    width: 100%;
+    height: 500px;
   }
 }
 </style>
