0060eed4cd0ecaab69de06ef78b7e14f3467087c
VMIAO-bx <212497659@qq.com>
2026-05-18 08:55:16 +0800

    二级分类--路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Layout from "@/views/Layout/layout-index.vue";
 import Login from "@/views/Login/login-index.vue";
 import Home from "@/views/Home/home-index.vue";
 import Category from "@/views/Category/category-index.vue";
+import SubCategory from "@/views/SubCategory/subCategory-index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +19,10 @@ const router = createRouter({
         {
           path: "category/:id",
           component: Category,
+        },
+        {
+          path: "category/sub/:id",
+          component: SubCategory,
         },
       ],
     },
