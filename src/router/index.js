2c4dddde5a9d155517bef9879e7be0a85224c3bf
VMIAO-bx <212497659@qq.com>
2026-05-18 16:50:36 +0800

    详情--基础页面的渲染

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Login from "@/views/Login/login-index.vue";
 import Home from "@/views/Home/home-index.vue";
 import Category from "@/views/Category/category-index.vue";
 import SubCategory from "@/views/SubCategory/subCategory-index.vue";
+import Detail from "@/views/Detail/detail-index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,6 +24,10 @@ const router = createRouter({
         {
           path: "category/sub/:id",
           component: SubCategory,
+        },
+        {
+          path: "detail/:id",
+          component: Detail,
         },
       ],
     },
