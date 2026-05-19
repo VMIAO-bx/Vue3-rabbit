cd357de59636bc519a70b6fa04912e81eee87173
VMIAO-bx <212497659@qq.com>
2026-05-19 17:38:48 +0800

    登录--数据持久化

--- src/stores/user.js
+++ src/stores/user.js
@@ -3,18 +3,25 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 import { loginAPI } from "@/apis/user";
 
-export const useUserStore = defineStore("user", () => {
-  //1.定义管理用户数据的state
-  const userInfo = ref();
-  //2.定义获取数据接口的action
-  const getUserInfo = async ({ account, password }) => {
-    const res = await loginAPI({ account, password });
-    userInfo.value = res.result;
-  };
+export const useUserStore = defineStore(
+  "user",
+  () => {
+    //1.定义管理用户数据的state
+    const userInfo = ref();
+    //2.定义获取数据接口的action
+    const getUserInfo = async ({ account, password }) => {
+      const res = await loginAPI({ account, password });
+      userInfo.value = res.result;
+    };
 
-  //3.以对象的格式把useInfo和getUserInfo以对象的格式 return出去
-  return {
-    userInfo,
-    getUserInfo,
-  };
-});
+    //3.以对象的格式把useInfo和getUserInfo以对象的格式 return出去
+    return {
+      userInfo,
+      getUserInfo,
+    };
+  },
+  {
+    //使用pinia实现持久化，从而实现刷新时仍保持登录状态
+    persist: true,
+  },
+);
