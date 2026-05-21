7bf983c2b72a477952a7a0831b3d82995a4acb44
VMIAO-bx <212497659@qq.com>
2026-05-21 10:08:32 +0800

    登录--token拦截器携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //导入el的弹出框以及样式，这里用来在用户登录失败/成功情况下弹出提示框
 import { ElMessage } from "element-plus";
 import "element-plus/theme-chalk/el-message.css";
-
+import { useUserStore } from "@/stores/user";
 //创建axios实例
 const httpInstance = axios.create({
   //根域名
@@ -15,6 +15,12 @@ const httpInstance = axios.create({
 //axios请求拦截器
 httpInstance.interceptors.request.use(
   (config) => {
+    //1.首先获取到用户的token
+    const userStore = useUserStore();
+    const token = userStore.userInfo.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
   (e) => Promise.reject(e),
