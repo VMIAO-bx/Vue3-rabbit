060bce0d2385b0823062eeed85ed5417e72e7643
VMIAO-bx <212497659@qq.com>
2026-05-23 11:29:31 +0800

    接口购物车--加入购物车实现

---
+++ src/apis/cart.js
@@ -0,0 +1,20 @@
+import httpInstance from "@/utils/http";
+
+//封装加入购物车接口
+export const insertCartAPI = ({ skuId, count }) => {
+  return httpInstance({
+    url: "/member/cart",
+    method: "POST",
+    data: {
+      skuId,
+      count,
+    },
+  });
+};
+
+//获取最新的购物车列表
+export const findNewCartListAPI = () => {
+  return httpInstance({
+    url: "/member/cart",
+  });
+};
--- src/stores/cart.js
+++ src/stores/cart.js
@@ -1,5 +1,7 @@
 import { defineStore } from "pinia";
 import { ref, computed } from "vue";
+import { useUserStore } from "./user";
+import { insertCartAPI, findNewCartListAPI } from "@/apis/cart";
 
 export const useCartStore = defineStore(
   "cart",
@@ -7,15 +9,28 @@ export const useCartStore = defineStore(
     //1.定义state
     const cartList = ref([]);
     //2.定义获取cartList的方法
-    const addCart = (goods) => {
-      //1.如果先前添加过该商品，则在此基础上加count
-      const item = cartList.value.find((item) => goods.skuId === item.skuId);
-      if (item) {
-        //找到了
-        item.count += goods.count;
+    const userStore = useUserStore();
+    const isLogin = computed(() => userStore.userInfo.token);
+    const addCart = async (goods) => {
+      const { skuId, count } = goods;
+      if (isLogin.value) {
+        //执行登录后添加到购物车的操作
+        await insertCartAPI({ skuId, count });
+        const res = await findNewCartListAPI();
+
+        //最新购物车列表覆盖本地购物车列表
+        cartList.value = res.result;
       } else {
-        //2.若没有添加过，则push进cartList
-        cartList.value.push(goods);
+        const item = cartList.value.find((item) => goods.skuId === item.skuId);
+        if (item) {
+          //执行未登录的加入购物车的操作
+          //找到了
+          //1.如果先前添加过该商品，则在此基础上加count
+          item.count += goods.count;
+        } else {
+          //2.若没有添加过，则push进cartList
+          cartList.value.push(goods);
+        }
       }
     };
 
@@ -39,6 +54,15 @@ export const useCartStore = defineStore(
       return cartList.value.reduce((a, c) => a + c.count * c.price, 0);
     });
 
+    //1.已选择商品的总数量
+    const selectedCount = computed(() =>
+      cartList.value.filter((item) => item.selected).reduce((a, c) => a + c.count, 0),
+    );
+    //2.已选择商品的总价格
+    const selectedPrice = computed(() =>
+      cartList.value.filter((item) => item.selected).reduce((a, c) => a + c.count * c.price, 0),
+    );
+
     //是否全选
     const isAll = computed(() => cartList.value.every((item) => item.selected));
     //全选框选中时/未选中时，所有商品的selected的值都要与该框保持一致
@@ -50,6 +74,8 @@ export const useCartStore = defineStore(
       allCount,
       allPrice,
       isAll,
+      selectedCount,
+      selectedPrice,
       addCart,
       delCart,
       singleCheck,
