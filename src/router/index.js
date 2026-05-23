9b24b22d3bc3c1968dd3af1a233d1646e07dffb1
VMIAO-bx <212497659@qq.com>
2026-05-23 16:26:13 +0800

    结算页--路由的配置及基础数据页的渲染

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Category from "@/views/Category/category-index.vue";
 import SubCategory from "@/views/SubCategory/subCategory-index.vue";
 import Detail from "@/views/Detail/detail-index.vue";
 import CartList from "@/views/CartList/cartlist-index.vue";
+import Checkout from "@/views/Checkout/checkout-index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -31,8 +32,12 @@ const router = createRouter({
           component: Detail,
         },
         {
-          path: "cartList",
+          path: "cartlist",
           component: CartList,
+        },
+        {
+          path: "checkout",
+          component: Checkout,
         },
       ],
     },
