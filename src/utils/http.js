12f1e3f41b8108707e93bb3426de38b1206231f2
VMIAO-bx <212497659@qq.com>
2026-06-20 22:18:47 +0800

    fix: API 改用 HTTPS 解决 Mixed Content 拦截

--- src/utils/http.js
+++ src/utils/http.js
@@ -9,7 +9,7 @@ import router from "@/router/index";
 //创建axios实例
 const httpInstance = axios.create({
   //根域名
-  baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
+  baseURL: "https://pcapi-xiaotuxian-front-devtest.itheima.net",
   //超时时间
   timeout: 10000,
 });
